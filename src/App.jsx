cf02a3ac25110c051e4566f1bfb0dbb4e9c85324
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-06-09 11:45:13 -0700

    moved routes out

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./Home";
 import NavBar from "./NavBar";
-import Menu from "./Menu";
-import ItemLoader from "./ItemLoader";
-import AddItemForm from "./AddItemForm";
-
+import RoutesList from "./RoutesList";
 import SnackOrBoozeApi from "./api";
 import "./App.css";
 
@@ -64,31 +59,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <main>
-        <Routes>
-          <Route
-            path="/"
-            element={<Home snacks={snacks} drinks={drinks} />} />
-          <Route
-            path="/snacks"
-            element={<Menu items={snacks} type="snacks" title="Snacks" />} />
-          <Route
-            path="/drinks"
-            element={<Menu items={drinks} type="drinks" title="Drinks" />} />
-          <Route
-            path="/snacks/:id"
-            element={<ItemLoader items={snacks} cantFindPath="/snacks" />} />
-          <Route
-            path="/drinks/:id"
-            element={<ItemLoader items={drinks} cantFindPath="/drinks" />} />
-          <Route
-            path="/add-item"
-            element={<AddItemForm addItem={addItem} />} />
-          <Route
-            path="*"
-            element={<p>Hmmm. I can't seem to find what you want.</p>} />
-        </Routes>
-      </main>
+      <RoutesList snacks={snacks} drinks={drinks} addItem={addItem} />
     </div>
   );
 }
